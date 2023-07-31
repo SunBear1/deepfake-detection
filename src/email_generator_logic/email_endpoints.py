9560f1ac645c6c84e24865b85e087415728ac2b5
Jakub Sachajko <37691739+eska5@users.noreply.github.com>
2023-07-31 18:49:47 +0200

    Update src/email_generator_logic/email_endpoints.py
    Lets goo
    
    Co-authored-by: Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>

--- src/email_generator_logic/email_endpoints.py
+++ src/email_generator_logic/email_endpoints.py
@@ -1,4 +1,6 @@
-""" 1 SEC MAIL """
+""" 
+1 SEC MAIL ENDPOINTS
+"""
 NEW_EMAILS = "https://www.1secmail.com/api/v1/?action=genRandomMailbox&count="
 CHECK_INBOXES = "https://www.1secmail.com/api/v1/?action=getMessages&login={}&domain={}"
 CHECK_EXACT_MESSAGES = "https://www.1secmail.com/api/v1/?action=readMessage&login={}&domain={}&id={}"
