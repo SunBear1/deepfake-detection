affbad8f1507b70d6aec1beb7818cc6dd654fd89
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2023-08-26 14:18:04 +0200

    add connection string tools for connection to azure storage

--- src/generator/endpoints/events.py
+++ src/generator/endpoints/events.py
@@ -32,10 +32,28 @@ async def events(payload: EventGridData):
     :return:
     """
     logger.info(f"Event grid event with payload {payload}")
-    event_data = json.loads(payload.event_data)
-    file_name = event_data["blobUrl"].split("/")[-1]
-    # deepfake_engine = Engine()
-    # audio_org_file = deepfake_engine.download_audio_file()
-    # audio_df_file = deepfake_engine.generate_fake_audio_elevenlabs(audio_org_file)
-    # deepfake_engine.upload_audio_file(audio_df_file)
-    return Response(status_code=status.HTTP_200_OK, content=file_name)
+    df_generation_provider = "test2"
+    deepfake_engine = Engine()
+    try:
+        event_data = json.loads(payload.event_data)
+        raw_file_name = event_data["blobUrl"].split("/")[-1]
+        audio_org_file = deepfake_engine.download_audio_file(blob_name=raw_file_name)
+
+        name, extension = raw_file_name.split(".")
+        prefix = deepfake_engine.get_current_file_id()
+        df_file_name = f"{prefix}_{name}_FAKE_{df_generation_provider}.{extension}"
+        org_file_name = f"{prefix}_{name}_ORGN_{df_generation_provider}.{extension}"
+
+        audio_df_file = deepfake_engine.generate_fake_audio_elevenlabs(audio_org_file)
+
+        deepfake_engine.upload_audio_file(blob_name=df_file_name, audio_df_file=audio_df_file,
+                                          blob_directory="deepfakes")
+        deepfake_engine.upload_audio_file(blob_name=org_file_name, audio_df_file=audio_org_file,
+                                          blob_directory="originals")
+
+        return Response(status_code=status.HTTP_200_OK,
+                        content=f"Deepfake for file {org_file_name} was created and uploaded as {df_file_name}.")
+    except Exception as e:
+        logger.error(f"Error while processing event grid event: {e}")
+        return Response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        content=f"Error while processing event grid event: {e}")
