dc332c096ce971bbefba78b38d5fe71dc5bfaf9b
Jakub Sachajko <kuba.sachajko@gmail.com>
2023-09-21 15:50:51 +0200

    \n removal

--- src/generator/elevenLabsApi/utils.py
+++ src/generator/elevenLabsApi/utils.py
@@ -5,8 +5,10 @@ def fileTXT_to_list(filenameAndFilepath):
     with open(filenameAndFilepath, 'r') as file:
         content = file.read()
         lines = content.split(';')
+        lines = [line.strip() for line in lines if line.strip()]
 
-    return [line.strip() for line in lines if line.strip()]
+
+    return [element.replace("\n", "") for element in lines]
     
 def getMP3_from_dir(MP3directory):
     mp3_files = [os.path.join(MP3directory, f) for f in os.listdir(MP3directory) if f.endswith('.mp3')]
