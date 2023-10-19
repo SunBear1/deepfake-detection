8cd16e3eb183652b814441c1b611e7a4c626d10a
Jakub Sachajko <kuba.sachajko@gmail.com>
2023-10-19 20:49:49 +0200

    F

--- src/scripts/11labs_generator/main.py
+++ src/scripts/11labs_generator/main.py
@@ -1,8 +1,9 @@
 import os
+from pydub import AudioSegment
 
 import requests
 
-PATH_TO_SOURCES_DIRECTORY = os.getenv("PATH_TO_SOURCES")
+# PATH_TO_SOURCES_DIRECTORY = os.getenv("PATH_TO_SOURCES")
 URL = "http://projektbadawczyserver.eastus2.cloudapp.azure.com/api/v1"
 
 
@@ -50,13 +51,40 @@ def generate_deepfake_from_existing_voice(voice_id: str, text: str) -> None:
     print(f"Deepfake from voice with id {voice_id} generated successfully. Voice has been saved in Azure storage "
           f"container.")
 
+def merge_flac_files(directory_path, output_file):
+    flac_files = [f for f in os.listdir(directory_path) if f.endswith(".flac")]
+    
+    if not flac_files:
+        print(f"No FLAC files found in {directory_path}.")
+        return
+    
+    combined = AudioSegment.empty()
+    
+    for file in flac_files:
+        audio_path = os.path.join(directory_path, file)
+        audio = AudioSegment.from_file(audio_path, format="flac")
+        combined += audio
+    
+    combined.export(output_file, format="flac")
+    print(f"Merged {len(flac_files)} FLAC files into {output_file}.")
 
 if __name__ == "__main__":
-    for file in os.listdir(PATH_TO_SOURCES_DIRECTORY):
-        if file.endswith(".txt"):
-            print(f"Processing file: {file}")
-            text_to_speak = prepare_text_file(path_to_file=os.path.join(PATH_TO_SOURCES_DIRECTORY, file))
-            created_voice_id = create_new_voice(
-                path_to_voice_file=os.path.join(PATH_TO_SOURCES_DIRECTORY, file.replace('.trans.txt', '.flac')))
-            generate_deepfake_from_existing_voice(voice_id=created_voice_id, text=text_to_speak)
-            delete_old_voice(old_voice_id=created_voice_id)
+    PATH_TO_SOURCES_DI0RECTORY = r"C:\Users\kubas\Downloads\LibriSpeech_notprocessed\LibriSpeech\dev-clean"
+    # wchodzę do 1 directory np pierwsze losowe liczby
+    for dir in os.listdir(PATH_TO_SOURCES_DI0RECTORY):
+        dir = PATH_TO_SOURCES_DI0RECTORY + os.sep + dir
+        if os.path.isdir(dir):
+            # wchodzę do drugiego directory np 2130
+            for subdir in os.listdir(dir):
+                subdir = dir + os.sep + subdir
+                # Generowanie flac mergea:
+                for file in os.listdir(subdir):
+                    if file.endswith(".txt"):
+                        print(f"Processing file: {file}")
+                        combinedFlacFile = subdir + os.sep + "MERGED_" + file.replace('.trans.txt', '.flac')
+                        merge_flac_files(subdir,subdir + combinedFlacFile)
+                        text_to_speak = prepare_text_file(path_to_file=os.path.join(subdir, file))
+                        created_voice_id = create_new_voice(
+                            path_to_voice_file=os.path.join(combinedFlacFile, file.replace('.trans.txt', '.flac')))
+                        generate_deepfake_from_existing_voice(voice_id=created_voice_id, text=text_to_speak)
+                        delete_old_voice(old_voice_id=created_voice_id)
